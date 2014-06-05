4f204b92f6591a29a98481bdb25f3a059e195374
Tim Burke <tburke@ringtaildesign.com>
2014-06-05 17:24:25 -0400

    More work towards redesign... Add in incidents container, make common SASS variables, add date to clock.

--- public/redesign/assets/js/app.js
+++ public/redesign/assets/js/app.js
@@ -2,15 +2,18 @@ var clock = (function() {
     var clockDiv = document.getElementById("clock");
     var updateClock = function() {
         var currentTime = new Date();
+        var currentMonth = currentTime.getMonth() + 1;
+        var currentDay = currentTime.getDay();
+        var currentYear = currentTime.getFullYear();
         var currentHours = currentTime.getHours();
         var currentMinutes = currentTime.getMinutes();
         var currentSeconds = currentTime.getSeconds();
         currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
         currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
-        var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-        currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-        currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-        var currentTimeString = currentHours + ":" + currentMinutes + " " + timeOfDay;
+        var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+        currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+        currentHours = (currentHours == 0) ? 12 : currentHours;
+        var currentTimeString = currentMonth + "/" + currentDay + "/" + currentYear + "&nbsp;&nbsp;&nbsp;" + currentHours + ":" + currentMinutes + " " + timeOfDay;
         clockDiv.innerHTML = currentTimeString;
     }
     updateClock();
