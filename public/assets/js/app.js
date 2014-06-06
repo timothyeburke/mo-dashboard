0f4da98fda15fa19eab15f6dd209e45d8224a744
Timothy Burke <tburke@ringtaildesign.com>
2014-06-06 07:22:40 -0400

    Add in weather icon stub out.  Some update to data.json file.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,31 +1,28 @@
+var weatherIcons = {
+	"clear-day":           "",
+	"clear-night":         "",
+	"rain":                "",
+	"snow":                "",
+	"sleet":               "",
+	"wind":                "",
+	"fog":                 "",
+	"cloudy":              "",
+	"partly-cloudy-day":   "",
+	"partly-cloudy-night": "",
+	"hail":                "", // Reserved for future use
+	"thunderstorm":        "", // Reserved for future use
+	"tornado":             ""  // Reserved for future use
+};
+
+
+
+// Angular Stuff below here:
+
 var dashboardApp = angular.module('dashboardApp', []);
 
 dashboardApp.controller('DashboardController', function($scope, $http) {
 	var getData = function () {
 		$scope.now = new Date();
-    	$http.get('data.json').success(function(data) {
-
-	        $scope.busses = [data.south, data.north];
-	        $scope.trains = [data.B35, data.B04, data.E02];
-	        $scope.trains.forEach(function (station) {
-	        	station.Predictions.forEach(function (train) {
-	        		if (train.Line != "RD" && 
-						train.Line != "GR" && 
-						train.Line != "YL" && 
-						train.Line != "SV" && 
-						train.Line != "SV" && 
-						train.Line != "OR") {
-	        			train.Line = "";
-	        		}
-	        	});
-	        });
-	        $scope.incidents = data.incidents;
-	        $scope.temperature = data.weather.temperature;
-
-	    });
-    };
-    getData();
-    setInterval(getData, 5000);
 		$http.get('data.json').success(function(data) {
 			$scope.incidents = data.incidents;
 			$scope.temperature = data.weather.temperature;
