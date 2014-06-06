01d346f5b124e4e7443e07d099e20424dafaaf09
Tim Burke <tburke@ringtaildesign.com>
2014-06-05 21:16:27 -0400

    Work in Angular and data binding.

--- public/redesign/assets/js/app.js
+++ public/redesign/assets/js/app.js
@@ -1,21 +1,16 @@
-var clock = (function() {
-    var clockDiv = document.getElementById("clock");
-    var updateClock = function() {
-        var currentTime = new Date();
-        var currentMonth = currentTime.getMonth() + 1;
-        var currentDay = currentTime.getDay();
-        var currentYear = currentTime.getFullYear();
-        var currentHours = currentTime.getHours();
-        var currentMinutes = currentTime.getMinutes();
-        var currentSeconds = currentTime.getSeconds();
-        currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-        currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
-        var timeOfDay = (currentHours < 12) ? "AM" : "PM";
-        currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
-        currentHours = (currentHours == 0) ? 12 : currentHours;
-        var currentTimeString = currentMonth + "/" + currentDay + "/" + currentYear + "&nbsp;&nbsp;&nbsp;" + currentHours + ":" + currentMinutes + " " + timeOfDay;
-        clockDiv.innerHTML = currentTimeString;
-    }
-    updateClock();
-    setInterval(updateClock, 1000);
-})();
+var dashboardApp = angular.module('dashboardApp', []);
+
+dashboardApp.controller('DashboardController', function($scope, $http) {
+	var getData = function () {
+		$scope.now = new Date();
+    	$http.get('data.json').success(function(data) {
+
+	        $scope.busses = [data.south, data.north];
+	        $scope.trains = [data.B35, data.B04];
+	        $scope.incidents = data.incidents;
+
+	    });
+    };
+    getData();
+    setInterval(getData, 5000);
+});
