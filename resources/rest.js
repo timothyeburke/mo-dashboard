89898753f98113555a4065a02a0f289580202c72
Tim Burke <timothy.e.burke@gmail.com>
2016-04-11 21:26:30 -0400

    Banish semicolons from rest.js.

--- resources/rest.js
+++ resources/rest.js
@@ -1,14 +1,14 @@
-var http     = require('http');
-var https    = require('https');
-var Forecast = require('forecast.io');
-var xml      = require('xml2js');
-var _        = require('lodash');
+var http     = require('http')
+var https    = require('https')
+var Forecast = require('forecast.io')
+var xml      = require('xml2js')
+var _        = require('lodash')
 
 var keys = JSON.parse(process.env.WMATA_API_KEYS)
 
 var getWmataApiKey = function () {
-	var key = keys[Math.floor(Math.random() * keys.length)];
-	return '&api_key=' + key + '&subscription-key=' + key;
+	var key = keys[Math.floor(Math.random() * keys.length)]
+	return '&api_key=' + key + '&subscription-key=' + key
 }
 
 module.exports = function (app) {
@@ -46,17 +46,17 @@ module.exports = function (app) {
 		car2go: [],
 		incidents: [],
 		weather: {}
-	};
+	}
 
 	var bbox = {
 		westLon:  -77.012500,
 		eastLon:  -76.997900,
 		southLat:  38.908567,
 		northLat:  38.920694
-	};
+	}
 
 	var isInBbox = function(lon, lat, westLon, eastLon, southLat, northLat) {
-		return westLon <= lon && lon <= eastLon && southLat <= lat && lat <= northLat;
+		return westLon <= lon && lon <= eastLon && southLat <= lat && lat <= northLat
 	}
 
 	var getBusPositions = function () {
@@ -66,88 +66,88 @@ module.exports = function (app) {
 				port: 443,
 				path: '/Bus.svc/json/jBusPositions?RouteID&Lat=38.9155&Lon=-77.0050&Radius=1600' + getWmataApiKey(),
 				method: 'GET'
-			};
+			}
 
-			var data = '';
+			var data = ''
 			var request = https.get(options, function (response) {
 				response.on('data', function (d) {
-					data += d;
-				});
+					data += d
+				})
 
 				response.on('end', function () {
 					try {
-						var busses = JSON.parse(data);
+						var busses = JSON.parse(data)
 
-						db.liveBusses.length = 0;
+						db.liveBusses.length = 0
 						busses.BusPositions.forEach(function (bus) {
-							bus.latitude = bus.Lat;
-							bus.longitude = bus.Lon;
-							db.liveBusses.push(bus);
-						});
+							bus.latitude = bus.Lat
+							bus.longitude = bus.Lon
+							db.liveBusses.push(bus)
+						})
 
 						db.liveBusses = _.filter(db.liveBusses, function (bus) {
-							return isInBbox(bus.Lon, bus.Lat, bbox.westLon, bbox.eastLon, bbox.southLat, bbox.northLat);
-						});
+							return isInBbox(bus.Lon, bus.Lat, bbox.westLon, bbox.eastLon, bbox.southLat, bbox.northLat)
+						})
 					} catch (err) {
-						console.log("Error parsing json.");
+						console.log("Error parsing json.")
 					}
-				});
+				})
 
 				response.on('error', function () {
-					console.log(new Date().toString() + " : Error getting live bus data.");
-				});
-			});
-		};
+					console.log(new Date().toString() + " : Error getting live bus data.")
+				})
+			})
+		}
 
-		fetchPositions();
-	};
+		fetchPositions()
+	}
 
-	getBusPositions();
-	setInterval(getBusPositions, 1000 * 10);
+	getBusPositions()
+	setInterval(getBusPositions, 1000 * 10)
 
 	var getBusPredictions = function () {
 		var errorObject = {
 			Predictions: [],
 			StopName: "Error fetching bus data."
-		};
+		}
 		var fetchPredictions = function (stop, direction) {
 			var options = {
 				hostname: 'api.wmata.com',
 				port: 443,
 				path: '/NextBusService.svc/json/jPredictions?StopID=' + stop + getWmataApiKey(),
 				method: 'GET'
-			};
-			var data = '';
+			}
+			var data = ''
 			var request = https.get(options, function(response) {
 				response.on('data', function (d) {
-					data += d;
-				});
+					data += d
+				})
 
 				response.on('end', function () {
 					try {
-						db[direction] = JSON.parse(data);
+						db[direction] = JSON.parse(data)
 					} catch (err) {
-						db[direction] = errorObject;
+						db[direction] = errorObject
 					}
 					if (db[direction] == undefined) {
-						db[direction] = errorObject;
+						db[direction] = errorObject
 					}
-				});
+				})
 
 				response.on('error', function () {
-					console.log(new Date().toString() + " : Error getting bus data.");
-				});
-			});
-			request.end();
-		};
-		fetchPredictions(1001624, "south");
-		// fetchPredictions(1001620, "north"); // P6 North - not really needed
-		fetchPredictions(1001425, "toUSt");
-		fetchPredictions(1001715, "G8West");
-		fetchPredictions(1003467, "G8East");
-	};
-	getBusPredictions();
-	setInterval(getBusPredictions, 1000 * 25);
+					console.log(new Date().toString() + " : Error getting bus data.")
+				})
+			})
+			request.end()
+		}
+		fetchPredictions(1001624, "south")
+		// fetchPredictions(1001620, "north") // P6 North - not really needed
+		fetchPredictions(1001425, "toUSt")
+		fetchPredictions(1001715, "G8West")
+		fetchPredictions(1003467, "G8East")
+	}
+	getBusPredictions()
+	setInterval(getBusPredictions, 1000 * 25)
 
 	var getIncidents = function () {
 		var options = {
@@ -155,67 +155,67 @@ module.exports = function (app) {
 			port: 443,
 			path: '/Incidents.svc/json/Incidents?' + getWmataApiKey(),
 			method: 'GET'
-		};
-		var temp = '';
+		}
+		var temp = ''
 		var request = https.get(options, function(response) {
 			response.on('data', function (d) {
-				temp += d;
-			});
+				temp += d
+			})
 
 			response.on('end', function () {
 				try {
-					db.incidents = JSON.parse(temp).Incidents;
+					db.incidents = JSON.parse(temp).Incidents
 					db.incidents.forEach(function (incident) {
-						incident.affected = _.compact(incident.LinesAffected.split(';'));
-						incident.Description = _.trim(incident.Description.replace(/(Blue|Orange|Red|Silver|Green|Yellow) Line\:/ig, ''));
-						console.log(incident.Description);
+						incident.affected = _.compact(incident.LinesAffected.split(''))
+						incident.Description = _.trim(incident.Description.replace(/(Blue|Orange|Red|Silver|Green|Yellow) Line\:/ig, ''))
+						console.log(incident.Description)
 						if (incident.Description.indexOf(".") != -1) {
-							incident.Description = incident.Description.split(".")[0] + ".";
+							incident.Description = incident.Description.split(".")[0] + "."
 						}
-					});
+					})
 				} catch (err) {
-					// db.incidents = [];
+					// db.incidents = []
 				}
 				if (db.incidents == undefined) {
-					db.incidents = [];
+					db.incidents = []
 				}
-			});
+			})
 
 			response.on('error', function () {
-				// db.incidents = [];
-				console.log(new Date().toString() + " : Error getting incident data.");
-			});
-		});
-		request.end();
-	};
-	getIncidents();
-	setInterval(getIncidents, 1000 * 60);
+				// db.incidents = []
+				console.log(new Date().toString() + " : Error getting incident data.")
+			})
+		})
+		request.end()
+	}
+	getIncidents()
+	setInterval(getIncidents, 1000 * 60)
 
 	var getWeather = function () {
-		var lon = -77.0033354;
-		var lat =  38.9152131;
-		var forecast_key = '2cb1727e2157365c87d67c621ec1bf43';
+		var lon = -77.0033354
+		var lat =  38.9152131
+		var forecast_key = '2cb1727e2157365c87d67c621ec1bf43'
 
 		var forecast = new Forecast({
 			APIKey: forecast_key
-		});
+		})
 
 		forecast.get(lat, lon, function (err, res, data) {
 			if (err) {
-				console.log(new Date().toString() + " : " + err);
-				db.weather = {};
+				console.log(new Date().toString() + " : " + err)
+				db.weather = {}
 			}
 			data.currently.low = _.min(data.hourly.data, function (hour) {
-				return hour.temperature;
-			});
+				return hour.temperature
+			})
 			data.currently.high = _.max(data.hourly.data, function (hour) {
-				return hour.temperature;
-			});
-			db.weather = data.currently;
-		});
-	};
-	getWeather();
-	setInterval(getWeather, 1000 * 60 * 10);
+				return hour.temperature
+			})
+			db.weather = data.currently
+		})
+	}
+	getWeather()
+	setInterval(getWeather, 1000 * 60 * 10)
 
 	var getTrainPredictions = function () {
 		var fetchPredictions = function (station) {
@@ -224,51 +224,51 @@ module.exports = function (app) {
 				port: 443,
 				path: '/StationPrediction.svc/json/GetPrediction/' + station + '?' + getWmataApiKey(),
 				method: 'GET'
-			};
-			var temp = '';
+			}
+			var temp = ''
 			var request = https.get(options, function(response) {
 				response.on('data', function (d) {
-					temp += d;
-				});
+					temp += d
+				})
 
 				response.on('end', function () {
 					try {
-						db[station].Predictions = JSON.parse(temp).Trains;
+						db[station].Predictions = JSON.parse(temp).Trains
 						db[station].Predictions.forEach(function (train) {
 							if (train.Line != "RD" && train.Line != "GR" &&
 								train.Line != "YL" && train.Line != "SV" &&
 								train.Line != "SV" && train.Line != "OR") {
-								train.Line = "";
+								train.Line = ""
 							}
-						});
+						})
 					} catch (err) {
-						// db[station].Predictions = [];
+						// db[station].Predictions = []
 					}
-				});
+				})
 
 				response.on('error', function () {
-					console.log(new Date().toString() + " : Error getting train data.");
-					// db[station].Predictions = [];
-				});
-			});
-			request.end();
-		};
-		fetchPredictions("B35");
-		fetchPredictions("B04");
-		fetchPredictions("E02");
-	};
-	getTrainPredictions();
-	setInterval(getTrainPredictions, 1000 * 60);
+					console.log(new Date().toString() + " : Error getting train data.")
+					// db[station].Predictions = []
+				})
+			})
+			request.end()
+		}
+		fetchPredictions("B35")
+		fetchPredictions("B04")
+		fetchPredictions("E02")
+	}
+	getTrainPredictions()
+	setInterval(getTrainPredictions, 1000 * 60)
 
 	var getBikeshareData = function () {
 		var processBikeshareXML = function (data) {
-			db.bikeshare.length = 0;
+			db.bikeshare.length = 0
 			xml.parseString(data, function(err, result) {
-				var stations = result.stations.station;
+				var stations = result.stations.station
 				stations.forEach(function (station) {
-					var id = station.id[0];
-					var lon = station.long[0];
-					var lat = station.lat[0];
+					var id = station.id[0]
+					var lon = station.long[0]
+					var lat = station.lat[0]
 					if (isInBbox(lon, lat, bbox.westLon, bbox.eastLon, bbox.southLat, bbox.northLat)) {
 						db.bikeshare.push({
 							id: station.id[0],
@@ -279,34 +279,34 @@ module.exports = function (app) {
 							longitude: station.long[0],
 							nbBikes: station.nbBikes[0],
 							nbEmptyDocks: station.nbEmptyDocks[0]
-						});
+						})
 					}
-				});
-			});
+				})
+			})
 		}
 		var options = {
 			hostname: 'www.capitalbikeshare.com',
 			port: 443,
 			path: '/data/stations/bikeStations.xml',
 			method: 'GET'
-		};
-		var temp = '';
+		}
+		var temp = ''
 		var request = https.get(options, function (response) {
 			response.on('data', function (d) {
-				temp += d;
-			});
+				temp += d
+			})
 
 			response.on('end', function () {
-				processBikeshareXML(temp);
-			});
+				processBikeshareXML(temp)
+			})
 
 			response.on('error', function (err) {
-				console.log(new Date().toString() + " : Error getting Bikeshare data.");
-			});
-		});
-	};
-	getBikeshareData();
-	setInterval(getBikeshareData, 1000 * 60);
+				console.log(new Date().toString() + " : Error getting Bikeshare data.")
+			})
+		})
+	}
+	getBikeshareData()
+	setInterval(getBikeshareData, 1000 * 60)
 
 	var getCar2GoData = function () {
 		var options = {
@@ -314,40 +314,40 @@ module.exports = function (app) {
 			port: 443,
 			path: '/api/v2.0/vehicles?loc=Washington%20DC&format=json',
 			method: 'GET'
-		};
-		var temp = '';
+		}
+		var temp = ''
 		var request = https.get(options, function(response) {
 			response.on('data', function (d) {
-				temp += d;
-			});
+				temp += d
+			})
 
 			response.on('end', function () {
 				try {
-					var car2go = JSON.parse(temp);
-					db.car2go.length = 0;
+					var car2go = JSON.parse(temp)
+					db.car2go.length = 0
 					car2go.placemarks.forEach(function (car) {
-						car.coordinates = eval(car.coordinates);
-						var lon = car.coordinates[0];
-						var lat = car.coordinates[1];
+						car.coordinates = eval(car.coordinates)
+						var lon = car.coordinates[0]
+						var lat = car.coordinates[1]
 						if (isInBbox(lon, lat, bbox.westLon, bbox.eastLon, bbox.southLat, bbox.northLat)) {
-							car.longitude = lon;
-							car.latitude  = lat;
-							db.car2go.push(car);
+							car.longitude = lon
+							car.latitude  = lat
+							db.car2go.push(car)
 						}
-					});
+					})
 				} catch (err) {
 				}
-			});
+			})
 
 			response.on('error', function () {
-				console.log(new Date().toString() + " : Error getting car2go data.");
-				db.car2go.length = 0;
-			});
-		});
-		request.end();
-	};
-	getCar2GoData();
-	setInterval(getCar2GoData, 1000 * 60);
+				console.log(new Date().toString() + " : Error getting car2go data.")
+				db.car2go.length = 0
+			})
+		})
+		request.end()
+	}
+	getCar2GoData()
+	setInterval(getCar2GoData, 1000 * 60)
 
 	// Data JSON REST endpoint
 	app.get('/data.json', function (req, res) {
@@ -359,20 +359,20 @@ module.exports = function (app) {
 			liveBusses: db.liveBusses,
 			trains:    [db.B35, db.B04, db.E02],
 			weather:   db.weather
-		};
+		}
 
-		res.contentType('application/json');
-		res.send(data);
-	});
+		res.contentType('application/json')
+		res.send(data)
+	})
 
 	app.get('/work.json', function (req, res) {
 		var data = {
 			busses: [db.G8East]
 		}
 
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "X-Requested-With");
-		res.contentType('application/json');
-		res.send(JSON.stringify(data, null, 4));
-	});
+		res.header("Access-Control-Allow-Origin", "*")
+		res.header("Access-Control-Allow-Headers", "X-Requested-With")
+		res.contentType('application/json')
+		res.send(JSON.stringify(data, null, 4))
+	})
 }
